5d92373c17cf3064efd64a6d00a21b8a84618dc4
Oliver Kirkup <koli182@protonmail.com>
2021-08-03 14:12:10 -0400

    Add project files.

---
+++ PoorlyDesignedCasino/Guy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoorlyDesignedCasino
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+      
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " has " + Cash + " dollars.");
+        }
+
+   
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " says: " +
+                    "You don't have " + amount + " dollars");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+      
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't an amount I'll take");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
---
+++ PoorlyDesignedCasino/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PoorlyDesignedCasino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random random = new Random();
+            double odds = random.Next(-50, 100);
+            Guy player = new Guy() { Cash = 1000, Name = "Player One" };
+            Console.WriteLine("Welcome to the Poorly Designed Casino, where the odds are almost never in your favor! Today's odds are: " + odds);
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+                Console.Write("Please place your bet: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = player.GiveCash(amount) * 2;
+                    if (pot > 0)
+                    {
+                        if (random.NextDouble() > odds)
+                        {
+                            int winnings = pot;
+                            Console.WriteLine("You have won: " + winnings + " dollars.");
+                            player.ReceiveCash(winnings);
+                        } else
+                        {
+                            Console.WriteLine("Sorry, you have lost. Better luck next time!");
+                        }
+                    }
+                } else
+                {
+                    Console.WriteLine("Please enter a valid bet amount.");
+                }
+                if (player.Cash == 0)
+                {
+                    Console.WriteLine("Uh Oh... your balance has reached zero dollars, might be time to sell the house?");
+
+                }
+            }
+        }
+    }
+}
